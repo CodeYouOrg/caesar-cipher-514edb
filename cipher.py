a2c208e373c398e73bed2bb4bb7f575e2f016c2a
vitiale <vlirianoluis@gmail.com>
2024-07-24 00:50:00 -0400

    Version 1.0

--- cipher.py
+++ cipher.py
@@ -1,1 +1,21 @@
 # add your code here
+from itertools import cycle
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def matchingLetter(letter):
+    if letter in alphabet :
+        longitud = alphabet.index(letter)+5
+        if longitud > 25:
+            letter = alphabet[longitud - 26]
+        else:
+            letter = alphabet[alphabet.index(letter)+5]
+    return letter    
+
+
+user_text = input('Please enter a senctence: ')
+user_text = user_text.lower()
+
+resp = ""
+for let in user_text:
+    resp = resp + matchingLetter(let)
+print('The encrypted sentence is:', resp)
